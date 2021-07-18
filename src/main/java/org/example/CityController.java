7c70061f0a5c4dcdbe921951fab39a3bf8ed33d8
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2021-07-18 15:10:46 -0300

    ### Added Ações para os botoes de salvar, buscar pelo ir e excluir

--- src/main/java/org/example/CityController.java
+++ src/main/java/org/example/CityController.java
@@ -1,34 +1,46 @@
 package main.java.org.example;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import main.java.org.example.dao.CitiesDAO;
 import main.java.org.example.model.Cities;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class CityController {
+
+    Cities cities = null;
 
     @FXML
     private TextField txtCity;
 
     @FXML
-    private Button btnSaveCity;
-
-    @FXML
-    private Button btnSearch;
-
-    @FXML
-    private Button btnDelete;
-
-
-    @FXML
-    private void saveCity() throws IOException {
+    private void btnSaveCity() throws IOException {
         CitiesDAO citiesDAO = new CitiesDAO();
-        Cities cities = new Cities();
+        cities = new Cities();
         cities.setName(txtCity.getText());
         citiesDAO.save(cities);
         App.setRoot("/main/resources/org/example/initial");
+    }
+
+    public void btnSearch() {
+        TextInputDialog dialog = new TextInputDialog("id");
+        dialog.setHeaderText("Digite o id da cidade:");
+        Optional<String> result = dialog.showAndWait();
+        Long id = Long.parseLong(result.get());
+
+        CitiesDAO citiesDAO = new CitiesDAO();
+        cities = citiesDAO.findById(id);
+        txtCity.setText(cities.getName());
+    }
+
+    public void btnDelete() {
+        CitiesDAO citiesDAO = new CitiesDAO();
+        if(cities != null){
+            citiesDAO.delete(cities.getId());
+            txtCity.setText("");
+        }
     }
 }
